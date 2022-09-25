0efd0540e4a43169c70bea1ef0dcbeeac88292d1
jacelynr <jvroan@gmail.com>
2022-09-25 18:51:30 -0500

    Displaying Repos from Github

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 const overview = document.querySelector(".overview");
 //set value to github username
 const username = "JacelynR";
+//select the ul
+const reposList = document.querySelector(".repo-list")
 
 const gitUserProfile = async function() {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
 
     const results = await userInfo.json();
-    console.log(results);
+    //console.log(results);
     displayUserInfo(results);
 };
-
 gitUserProfile();
 
 const displayUserInfo = function(results) {
@@ -31,5 +32,25 @@ const displayUserInfo = function(results) {
         </div>
     `;
     overview.append(div);
+    getRepos();
+};
 
+const getRepos = async function() {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+
+    const data = await fetchRepos.json();
+    //console.log(data);
+
+    showRepos(data);
+};
+
+const showRepos = function(repos) {
+
+    for (let repo of repos) {
+        const repoItem = document.createElement("li");
+
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        reposList.append(repoItem);
+    }
 };
